08c0d0fdf04d288f7a346254e1f71bf1d049746e
Elliot Murphy <statik@users.noreply.github.com>
2021-01-22 16:38:06 -0500

    Use skylib copy_file rule instead of tarfile tricks
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -6,7 +6,7 @@ mkdir -p dist/vbs_darwin_amd64
 mkdir -p dist/vbs_linux_amd64
 mkdir -p dist/vbs_windows_amd64
 
-sudo cp bdist/vbs-darwin dist/vbs_darwin_amd64/vbs
-sudo cp bdist/vbs-linux dist/vbs_linux_amd64/vbs
-sudo cp bdist/vbs-windows.exe dist/vbs_windows_amd64/vbs.exe
+sudo cp bazel-bin/bdist/vbs-darwin dist/vbs_darwin_amd64/vbs
+sudo cp bazel-bin/bdist/vbs-linux dist/vbs_linux_amd64/vbs
+sudo cp bazel-bin/bdist/vbs-windows.exe dist/vbs_windows_amd64/vbs.exe
 
