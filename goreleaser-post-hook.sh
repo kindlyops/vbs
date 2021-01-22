cdfb0a75b61a2d378eae14c321bc0413af466893
Elliot Murphy <statik@users.noreply.github.com>
2021-01-22 17:04:26 -0500

    adjust paths
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -6,7 +6,7 @@ mkdir -p dist/vbs_darwin_amd64
 mkdir -p dist/vbs_linux_amd64
 mkdir -p dist/vbs_windows_amd64
 
-sudo cp bazel-bin/bdist/vbs-darwin dist/vbs_darwin_amd64/vbs
-sudo cp bazel-bin/bdist/vbs-linux dist/vbs_linux_amd64/vbs
-sudo cp bazel-bin/bdist/vbs-windows.exe dist/vbs_windows_amd64/vbs.exe
+cp bazel-bin/bdist/vbs_darwin_amd64/vbs dist/vbs_darwin_amd64/vbs
+cp bazel-bin/bdist/vbs_linux_amd64/vbs dist/vbs_linux_amd64/vbs
+cp bazel-bin/bdist/vbs_windows_amd64/vbs.exe dist/vbs_windows_amd64/vbs.exe
 
